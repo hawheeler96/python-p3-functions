7326502b0b4cffade502197c5b491519f0a9f8bc
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-12-09 16:21:18 -0800

    lab complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Holly")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(1,2)
 
 def halve(number):
-    pass
+    return number/2
+
+halve(4)
